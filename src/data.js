81e326321fb64bece1307051f2d948ca83499300
Mohammad Anas <82320689+Mohd-anas0693@users.noreply.github.com>
2024-07-20 23:21:15 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,5 +1,5 @@
 export const data =  [
-        { id: 1, name: "Page 1", isCompleted: true },
+        { id: 1, name: "Page 1", isCompleted: false },
         { id: 2, name: "Page 2", isCompleted: false },
         { id: 3, name: "Page 3", isCompleted: false },
         { id: 4, name: "Page 4", isCompleted: false },
